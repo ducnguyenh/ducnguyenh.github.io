bebf24f4ad45dfcdf43d0964e5dcb2f079e52794
unknown <nguyenhuuduc8@gmail.com>
2019-05-20 21:51:15 +0700

    Update homeWork.js

--- 07---loopForWhile/homeWork.js
+++ 07---loopForWhile/homeWork.js
@@ -1,33 +1,33 @@
 //Bài 1: Cho 1 chuỗi số, hãy viết hàm có tác dụng sao chép chuỗi số lên 10 lần, ngăn cách nhau bởi ký tự "-".
 function copyCat(sequence) {
     var copyArray = [];
-    for (var i=0; i < 10; i++) {
-        copyArray.push(sequence)
+    for (var i = 0; i < 10; i++) {
+        copyArray.push(sequence);
     }
-    return copyArray.join("-")
+    return copyArray.join("-");
 }
 
 //Bài 2. Cho 1 số nguyên n. Viết hàm tính n giai thừa (n!)
 function calFactorial(n) {
-    if (n==0 || n==1) {
-        return 1
+    if (n == 0 || n == 1) {
+        return 1;
     }
-    return calFactorial(n-1)*n
+    return calFactorial(n - 1) * n;
 }
 
 //Bài 3. Cho 1 chuỗi, hãy viết hàm đảo ngược chuỗi đó.
-function reSequence (sequence) {
+function reSequence(sequence) {
     var fakeArray = [];
     for (var i = sequence.length; i >= 0; i--) {
         fakeArray.push(sequence[i]);
     }
-    return fakeArray.toString().replace(/,/g, '');
+    return fakeArray.toString().replace(/,/g, "");
 }
 
 //Bài 4. Cho 1 mảng tên của n học viên. Viết function sắp xếp lại thứ tự các học viên theo bảng chữ cái và in ra màn hình danh sách học viên kèm theo số thứ tự
 function traineeList(list) {
     for (var i = 0; i < list.length; i++) {
-        document.write("<p>" + (i + 1) + '. ' + list.sort()[i] + "</p>");
+        document.write("<p>" + (i + 1) + ". " + list.sort()[i] + "</p>");
     }
     return true;
 }
@@ -38,16 +38,16 @@ function newArray(list) {
     for (var i in list) {
         fakeArray.push(i % 2);
     }
-    return fakeArray
+    return fakeArray;
 }
 
 //Bài 6. Chỉ dùng vòng lặp for hoặc while, viết function in ra màn hình các số từ 1 đến 100 sao cho những giá trị là chẵn sẽ có màu xanh, những giá trị là lẻ có màu đỏ.
 function colorScreen(n) {
     for (var i = 1; i <= n; i = i + 2) {
         document.write("<span style='color:red'>" + i + " </span>" + "<br />");
-        document.write("<span style='color:green'>" + (i + 1) + "</span>" + "<br />");
-        }
-        return true;
+        document.write(
+            "<span style='color:green'>" + (i + 1) + "</span>" + "<br />"
+        );
     }
-
-
+    return true;
+}
