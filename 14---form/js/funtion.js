7782040636a4961fb581e6bbe51ee57d49558f66
unknown <nguyenhuuduc8@gmail.com>
2019-06-16 12:29:00 +0700

    Update funtion.js

--- 14---form/js/funtion.js
+++ 14---form/js/funtion.js
@@ -1,10 +1,15 @@
+$('.btn .btn-primary').on('click', function () {
+    console.log($('#inputText').val(),
+        $('#inputEmail').val(),
+        $('#mobile').val(),
+        $('input[name=gender]:checked', '#sign-up').val(),
+        $('#datepicker').val(),
+        $('#inputAddress').val(),
+        $('#inputPassword').val()),
+        $('#confirmPassword').val()
+});
 
-$('#inputText').val()
-$('#inputEmail').val()
-$('#mobile').val()
-$('input[name=gender]:checked', '#sign-up').val()
 
-$('#datepicker').val()
-$('#inputAddress').val()
-$('#inputPassword').val()
+
+
 
