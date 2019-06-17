8dabdc932662961a79a193a211188be63e61b808
unknown <nguyenhuuduc8@gmail.com>
2019-06-18 01:25:32 +0700

    Update funtion.js

--- 14---form/js/funtion.js
+++ 14---form/js/funtion.js
@@ -15,12 +15,13 @@ $(function () {
     $('#sign-up').attr("action", "success.html");
 
     function getRecylingUrl() {
-        let process1 = decodeURIComponent($(location).attr("href"));
+        let process1 = $(location).attr("href");
         let process2 = process1.slice(process1.indexOf('?') + 1);
         let process3 = process2.replace(/\+/g, " ").split('&');
         let result = '';
         for (let i = 0; i < process3.length - 1; i++) {
-            let titleUpCase = process3[i].replace(process3[i].charAt(0), process3[i].charAt(0).toUpperCase());
+            let decodeInfo = decodeURIComponent(process3[i]);
+            let titleUpCase = decodeInfo.replace(decodeInfo.charAt(0), decodeInfo.charAt(0).toUpperCase());
             result += '<p>' + titleUpCase.replace('=', ': ') + '</p>';
         }
         return result;
