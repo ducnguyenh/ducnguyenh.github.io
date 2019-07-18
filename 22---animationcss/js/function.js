05ba511a3f4d94a9345c5d50e05e42e29c10fd53
unknown <nguyenhuuduc8@gmail.com>
2019-07-19 02:09:09 +0700

    Create function.js

---
+++ 22---animationcss/js/function.js
@@ -0,0 +1,15 @@
+function fun2() {
+    window.scrollBy(0, 450)
+}
+setTimeout(fun2, 5000);
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function () {
+    $('#slideshow > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slideshow');
+}, 5000);
