29c5a39dc48ba54a6ae941ccb5443c76c6956e40
unknown <nguyenhuuduc8@gmail.com>
2019-09-21 11:26:38 +0700

    Update function.js

--- vmb-home4.0/js/function.js
+++ vmb-home4.0/js/function.js
@@ -64,4 +64,20 @@ $(function () {
             }
         ]
     })
+
+    $('.banner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        focusOnSelect: true,
+    });
+
+    $('.left-option').slick({
+        slidesToShow: 1,
+        dots: true,
+        arrows: false,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 })
