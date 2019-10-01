8a65c0260b5196bf60946fb914780a3797478096
Nguyen Huu Duc <nguyenhuuduc8@gmail.com>
2019-10-01 14:57:08 +0700

    update-banner

--- angular-coffee/src/app/app.module.ts
+++ angular-coffee/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { BannerComponent } from './shared/components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
