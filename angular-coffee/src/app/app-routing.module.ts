b1e7092f864e58ed11591d1066a979421d2ede57
unknown <nguyenhuuduc8@gmail.com>
2019-10-29 22:54:49 +0700

    Update app-routing.module.ts

--- angular-coffee/src/app/app-routing.module.ts
+++ angular-coffee/src/app/app-routing.module.ts
@@ -1,39 +1,49 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
+    path: "",
+    loadChildren: () =>
+      import("./components/home/home.module").then(m => m.HomeModule)
   },
   {
-    path: 'about-us',
-    loadChildren: () => import('./components/about-us/about-us.module').then(m => m.AboutUsModule)
+    path: "about-us",
+    loadChildren: () =>
+      import("./components/about-us/about-us.module").then(m => m.AboutUsModule)
   },
   {
-    path: 'services',
-    loadChildren: () => import('./components/all-services/all-services.module').then(m => m.AllServicesModule)
+    path: "services",
+    loadChildren: () =>
+      import("./components/all-services/all-services.module").then(
+        m => m.AllServicesModule
+      )
   },
   {
-    path: 'menu',
-    loadChildren: () => import('./components/drink-menu/drink-menu.module').then(m => m.DrinkMenuModule)
+    path: "menu",
+    loadChildren: () =>
+      import("./components/drink-menu/drink-menu.module").then(
+        m => m.DrinkMenuModule
+      )
   },
   {
-    path: 'gallery',
-    loadChildren: () => import('./components/gallery/gallery.module').then(m => m.GalleryModule)
+    path: "gallery",
+    loadChildren: () =>
+      import("./components/gallery/gallery.module").then(m => m.GalleryModule)
   },
   {
-    path: 'contact-us',
-    loadChildren: () => import('./components/contact-us/contact-us.module').then(m => m.ContactUsModule)
+    path: "contact-us",
+    loadChildren: () =>
+      import("./components/contact-us/contact-us.module").then(
+        m => m.ContactUsModule
+      )
   },
 
-  { path: '', redirectTo: '/', pathMatch: 'full' },
-
+  { path: "", redirectTo: "/", pathMatch: "full" }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
