98bc6339dc2e841ca2c6b9d329507d6bcd83819c
ducnguyenh <nguyenhuuduc8@gmail.com>
2020-04-09 12:52:14 +0700

    update style

--- vinSchool/body/js/main_body.js
+++ vinSchool/body/js/main_body.js
@@ -26,21 +26,21 @@ $(function () {
         },
       },
       {
-        breakpoint: 768,
+        breakpoint: 992,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 576,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 576,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
